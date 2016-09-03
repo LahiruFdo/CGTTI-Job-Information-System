5950faebbdc781db9bef21e97ac95391dd6b6a4a
MSNusry <msnusry@gmail.com>
2016-09-03 12:52:43 +0530

    can add details of material

--- Section/MupdateJob.php
+++ Section/MupdateJob.php
@@ -9,9 +9,15 @@ session_start();
         $machHr = $_POST['machHour'];
         $nMH = $oMH + $manHr ;
         $nMcnH = $oMcnH + $machHr ;
+        $code = "GT";
+        $Mat = $_POST['mat'];
+        $qun = $_POST['quan'];
+        $upr = $_POST['upr'];
    $sql="UPDATE job set man_hrs='$nMH' , mach_hrs='$nMcnH' where job_no='$jobNo'";
     $rst=mysqli_query($conn,$sql);
+    $sql1 = "INSERT INTO `cgtti`.`materials` (`ID`, `code`, `name`, `quantity`, `job_no`) VALUES (NULL, '$code', '$Mat', '$qun', '$jobNo')";
+    $rst1=mysqli_query($conn,$sql1);
 header('Location:Msection.php');
 //echo $nMH."--".$nMcnH;
-    
+   // echo $Mat."--".$qun."--".$upr;
 ?>
